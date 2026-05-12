62317c79b4f87873d812fe099c3d35dbed18f7b3
Siphesihle Shezi <hshezi023@student.wethinkcode.co.za>
2026-05-12 14:25:59 +0200

    Testing env creation with API key

--- videostatistics.py
+++ videostatistics.py
@@ -134,3 +134,4 @@ if __name__ == "__main__":
     video_ids = get_video_ids(playlistId)
     video_data = extract_video_data(video_ids)
     save_to_json(video_data)
+    print("Data extraction and saving completed successfully.")
